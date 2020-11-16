19abcaecbecdf4fa8630f1ec3bc6bc5bdb00b67c
Marcin <mnowinski01@protonmail.com>
2020-11-16 12:32:01 +0100

    improvements to responsive design

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
         <title>Log in</title>
         <meta charset="UTF-8"/>
         <link rel="stylesheet" type="text/css" href="main.css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700&display=swap"
             rel="stylesheet">
     </head>
