19c1211abef68d4b10b809f5a18b77b9e9edcc5f
Marcin <mnowinski01@protonmail.com>
2020-11-27 15:04:47 +0100

    changes

--- welcome.php
+++ welcome.php
@@ -22,8 +22,8 @@
     <body>
         <div id="welcome_main">
             <h1>You did it!</h1>
-            <h1>Now you can log in to your account</h1>
-            <div style="postion: absolute; width: 50%; height: 7%; background: white; z-index: 1;"></div>
+            <h2>Now you can log in to your account</h2>
+            <!-- <div style="postion: absolute; width: 100%; height: 7%; background: yellow; z-index: 1;"></div> -->
             <img src="img/illustrations/high_five.svg"/>
             <a class="button" href="index.php">Log in</a>
         </div>
