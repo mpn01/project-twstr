f1f7d877bcc5e677da5aee2b3a21aad1ec8f41f5
Marcin <mnowinski01@protonmail.com>
2020-12-09 19:44:31 +0100

    adding users.php

--- profile.php
+++ profile.php
@@ -60,6 +60,11 @@
                               <line x1="12" y1="17" x2="12" y2="17.01" />
                               <path d="M12 13.5a1.5 1.5 0 0 1 1 -1.5a2.6 2.6 0 1 0 -3 -4" />
                             </svg>Help</a>
+                            <a class="profile_content_menu_button" href="users.php"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="44" height="44" viewBox="0 0 24 24" stroke-width="2" stroke="#545454" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <circle cx="12" cy="7" r="4" />
+                            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                          </svg>Users</a>
                                 </div>
                                 <a class='button' href='logout.php'>Logout</a>
                             </div>
