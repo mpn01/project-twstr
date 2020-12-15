3cefa58c9146109a6076f43145a9523fae2bcb90
Marcin <mnowinski01@protonmail.com>
2020-12-15 20:26:41 +0100

    small fixes to text and design&syntax of inputs

--- welcome.php
+++ welcome.php
@@ -2,12 +2,12 @@
     session_start();
 
     //sprawdzanie czy użytkownik jest zalogowany, jeśli jest, przenosi go do profile.php
-     if(!isset($_SESSION['register_complete'])) {
-        header('Location: index.php');
-        exit();
-    } else {
-        unset($_SESSION['register_complete']);
-    }
+    //  if(!isset($_SESSION['register_complete'])) {
+    //     header('Locatiodex.phpn: index.php');
+    //     exit();
+    // } else {
+    //     unset($_SESSION['register_complete']);
+    // }
 ?>
 <html>
     <head>
@@ -21,11 +21,16 @@
     </head>
     <body>
         <div id="welcome_main">
-            <h1>You did it!</h1>
-            <h2>Now you can log in to your account</h2>
+            <h1 id="header1">You did it!</h1>
+            <h2 id="header2">Now you can log in to your account.</h2>
             <!-- <div style="postion: absolute; width: 100%; height: 7%; background: yellow; z-index: 1;"></div> -->
             <img src="img/illustrations/high_five.svg"/>
             <a class="button" href="index.php">Log in</a>
         </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
+        <script>
+            const header1 = document.getElementyById("header1");
+            const header2 = document.getElemenetById("header2");
+        </script>
     </body>
 </html>
